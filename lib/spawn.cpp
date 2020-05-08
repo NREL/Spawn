NREL/Spawn

commit c6f93ec2c307906d7179d654ed345a010bc24e55

Set temp of controlled zones during warmup
In spawn there is the concept of connected and unconnected zones.
Connected zones are those that are exposed through the FMU interface and
are able to have an HVAC system attached through the FMU. Unconnected zones
are not exposed through the FMU, although zone related output variables can
be requested, however the FMU does not establish the zone temperature
like in connected zones.

Previously, the mixed air temperature of even "connected" zones was computed
by EnergyPlus during warmup. Connected zones were treated like
unconnected zones, which means warmup iterations were performed until
the zone air temperature stablized.

With this change, the temperature of connected zones is set by the FMU,
and EnergyPlus warmup does not interfere with the temperature
established by the FMU.

lib/spawn.cpp
@@ -363,46 +363,37 @@ void Spawn::externalHVACManager() {
   EnergyPlus::ZoneTempPredictorCorrector::InitZoneAirSetPoints();
 
   // At this time, there is no data exchange or any other
-  // interaction with the FMU during wramup and sizing.
-  if( EnergyPlus::DataGlobals::DoingSizing || EnergyPlus::DataGlobals::WarmupFlag ) {
-    if(! EnergyPlus::DataGlobals::KickOffSimulation) {
-      updateZoneTemperatures(false); // false means don't skip connected zones, update all zones
-    }
-    return;
-  }
-
+  // interaction with the FMU while KickOffSimulation is true.
   if( EnergyPlus::DataGlobals::KickOffSimulation ) {
     return;
   }
 
+  // Exchange data with the FMU
+  exchange();
+
   // Only signal and wait for input if the current sim time is greather than or equal
   // to the requested time
-  if( currentSimTime() < requestedTime ) {
-    // Exchange data so that FMU inputs are retained,
-    // because EnergyPlus may have overwrote an input value
-    exchange();
-    return;
-  }
-
-  // Signal the end of the step
-  {
-    std::unique_lock<std::mutex> lk(control_mutex);
-    if (epstatus != ::spawn::EPStatus::TERMINATE)
+  if( currentSimTime() >= requestedTime ) {
+    // Signal the end of the step
     {
-     epstatus = ::spawn::EPStatus::NONE;
+      std::unique_lock<std::mutex> lk(control_mutex);
+      if (epstatus != ::spawn::EPStatus::TERMINATE)
+      {
+       epstatus = ::spawn::EPStatus::NONE;
+      }
     }
+
+    control_cv.notify_one();
+
+    // Wait for the epstatus to advance again
+    std::unique_lock<std::mutex> lk(control_mutex);
+    control_cv.wait(lk, [&]() {
+      return (
+        (epstatus == ::spawn::EPStatus::ADVANCE) ||
+        (epstatus == ::spawn::EPStatus::TERMINATE)
+      );
+    });
   }
-
-  control_cv.notify_one();
-
-  // Wait for the epstatus to advance again
-  std::unique_lock<std::mutex> lk(control_mutex);
-  control_cv.wait(lk, [&]() {
-    return (
-      (epstatus == ::spawn::EPStatus::ADVANCE) ||
-      (epstatus == ::spawn::EPStatus::TERMINATE)
-    );
-  });
 }
 
 boost::filesystem::path exedir() {

examples/sim.py
@@ -11,6 +11,11 @@ final_time = 60 * 10
 print("instantiate")
 model.instantiate(name='Model', visible=True)
 
+# It is important to set zone temperature before setup_experiment
+# in order for zones to be properly initialized during EnergyPlus warmup
+temp = 296.15
+model.set('Core_ZN_T', temp);
+
 print("setup_experiment")
 model.setup_experiment(start_time=start_time, stop_time=final_time)
 
@@ -45,7 +50,6 @@ for x in outputs.keys():
 # perform siulations
 t = start_time
 last_time = start_time
-temp = 296.15
 densityAir = 1.276; # kg/m^3
 heatCapacity = 1000.6; # J/kgK
 volume = model.get('Core_ZN_V')[0]

commit 8d293dc2e9faf81fd4aa72a3826123d9ed8d3011

Fix unwanted FMU communication during warmup
The FMU should not return from setupExperiment until after warmup. There
was a regression in c6f93ec2c307906d7179d654ed345a010bc24e55 that caused
a regression.

lib/spawn.cpp
@@ -364,12 +364,20 @@ void Spawn::externalHVACManager() {
 
   // At this time, there is no data exchange or any other
   // interaction with the FMU while KickOffSimulation is true.
+  // Sensors and actuators may not be setup at this point, so exchange
+  // might trigger exceptions
   if( EnergyPlus::DataGlobals::KickOffSimulation ) {
     return;
   }
 
   // Exchange data with the FMU
   exchange();
+
+  // There is no interaction with the FMU during warmup,
+  // so return now before signaling
+  if( EnergyPlus::DataGlobals::DoingSizing || EnergyPlus::DataGlobals::WarmupFlag ) {
+    return;
+  }
 
   // Only signal and wait for input if the current sim time is greather than or equal
   // to the requested time