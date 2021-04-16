NREL/Spawn

commit 5153da35bd527272c9c152747a3a7998b7a0e523

Be more strategic about exchanging data with EnergyPlus
This change avoids unnessecary calls to ::exchange if the input has not
chnaged. This avoids extra calls the EnergyPlus surface heat balance
calc. The standard numeric limits epsilon is used to define the
threshold to detect an input change.

A separate commit will make a Spawn configuration option to control the
surface heat balance convergence criteria.

ref lbl-srg/modelica-buildings#2437

lib/spawn.cpp
@@ -163,9 +163,8 @@ void Spawn::setStartTime(const double & time) {
 void Spawn::setTime(const double & time)
 {
   isRunningCheck();
-
   requestedTime = time;
-  exchange();
+  exchange(true);
 
   if(requestedTime >= nextEventTime()) {
     iterate();
@@ -184,8 +183,12 @@ double Spawn::nextEventTime() const {
 
 void Spawn::setValue(const unsigned int & ref, const double & value) {
   auto var = variables.find(ref);
-  if( var != variables.end() ) {
-    var->second.setValue(value, spawn::units::UnitSystem::MO);
+  if (var != variables.end()) {
+    const auto & cur_val = var->second.getValue(spawn::units::UnitSystem::MO);
+    if (std::abs(cur_val - value) > std::numeric_limits<float>::epsilon()) {
+      need_update = true;
+      var->second.setValue(value, spawn::units::UnitSystem::MO);
+    }
   } else {
     throw std::runtime_error(fmt::format("Attempt to set a value using an invalid reference: {}", ref));
   }
@@ -434,9 +437,11 @@ double Spawn::getSensorValue(Variable & var) {
   return getVariableValue(simState(), h);
 }
 
-void Spawn::exchange()
+void Spawn::exchange(const bool force)
 {
   isRunningCheck();
+
+  if (! force && ! need_update) return;
 
   auto actuateVar = [&](const Variable & var) {
     if( var.isValueSet() ) {
@@ -495,11 +500,6 @@ void Spawn::exchange()
   }
 
   // Run some internal EnergyPlus functions to update outputs
-  // Surface managers might be expensive. Need to investigate and consider being more strategic
-  //
-  // These two functions are not idempotent and they are causing,
-  // "Test SingleFamilyHouse Idempotence" to fail!
-  // FIX ME !!!
   EnergyPlus::HeatBalanceSurfaceManager::CalcHeatBalanceOutsideSurf(sim_state);
   EnergyPlus::HeatBalanceSurfaceManager::CalcHeatBalanceInsideSurf(sim_state);
 
@@ -562,6 +562,8 @@ void Spawn::exchange()
         break;
     }
   }
+
+  need_update = false;
 }
 
 void Spawn::initZoneEquip() {
@@ -590,7 +592,7 @@ void Spawn::externalHVACManager(EnergyPlusState state) {
   }
 
   // Exchange data with the FMU
-  exchange();
+  exchange(true);
 
   // There is no interaction with the FMU during warmup,
   // so return now before signaling

lib/spawn.hpp
@@ -108,7 +108,7 @@ public:
   void setLogCallback(std::function<void(EnergyPlus::Error, const std::string &)> cb);
   void logMessage(EnergyPlus::Error level, const std::string & message);
 
-  void exchange();
+  void exchange(const bool force = false);
 
 private:
   std::string instanceName;
@@ -118,6 +118,7 @@ private:
 
   double m_startTime{0.0};
   double requestedTime;
+  bool need_update{true};
 
   // Signal EnergyPlus to move through the simulation loop
   // Depending on the current simulation time, this may be an inner most hvac iteration,

commit 9b1d5e0243cee95439709f782c8071d8d79b2b0d

Fix error related to initializing inputs to 0.0
This was a regression resulting from https://github.com/NREL/Spawn/commit/5153da35bd527272c9c152747a3a7998b7a0e523

ref #lbl-srg/modelica-buildings#2450

lib/spawn.cpp
@@ -186,7 +186,8 @@ void Spawn::setValue(const unsigned int & ref, const double & value) {
   auto var = variables.find(ref);
   if (var != variables.end()) {
     const auto & cur_val = var->second.getValue(spawn::units::UnitSystem::MO);
-    if (std::abs(cur_val - value) > std::numeric_limits<float>::epsilon()) {
+    if (std::abs(value) <= std::numeric_limits<float>::epsilon() ||
+        std::abs(cur_val - value) > std::numeric_limits<float>::epsilon()) {
       need_update = true;
       var->second.setValue(value, spawn::units::UnitSystem::MO);
     }
@@ -425,6 +426,9 @@ int Spawn::getActuatorHandle(const std::string & componenttype, const std::strin
 
 void Spawn::setActuatorValue(const std::string & componenttype, const std::string & controltype, const std::string & componentname, const Real64 & value) {
   const auto h = getActuatorHandle(componenttype, controltype, componentname);
+  if (value == 0.0) {
+    ::setActuatorValue(simState(), h, 1.0);
+  }
   ::setActuatorValue(simState(), h, value);
 };
 

test/CMakeLists.txt
@@ -23,6 +23,7 @@ add_executable(tests
   test_temp_directory.cpp
   test_unzip_file.cpp
   test_floating_zone.cpp
+  test_single_family_house_lights_control.cpp
   test_fmi.cpp
   test_mbl_idf.cpp
 )

test/test_single_family_house_lights_control.cpp
@@ -1,0 +1,127 @@
+#include "../fmu/fmu.hpp"
+#include "../fmu/modeldescription.hpp"
+#include "../fmu/logger.h"
+#include "../util/filesystem.hpp"
+#include "../util/math.hpp"
+#include "paths.hpp"
+#include "create_epfmu.hpp"
+#include <catch2/catch.hpp>
+#include <nlohmann/json.hpp>
+#include <iostream>
+
+using json = nlohmann::json;
+
+TEST_CASE("Test SingleFamilyHouse Lights")
+{
+  std::string spawn_input_string = fmt::format(
+  R"(
+    {{
+      "version": "0.1",
+      "EnergyPlus": {{
+        "idf": "{idfpath}",
+        "weather": "{epwpath}",
+        "relativeSurfaceTolerance": 1.0e-10
+      }},
+      "fmu": {{
+          "name": "MyBuilding.fmu",
+          "version": "2.0",
+          "kind"   : "ME"
+      }},
+      "model": {{
+        "zones": [
+           {{ "name": "LIVING ZONE" }}
+        ],
+        "schedules": [
+           {{
+              "name": "HOUSE LIGHTING",
+              "unit": "1",
+              "fmiName": "lighting input"
+           }}
+        ],
+        "outputVariables": [
+          {{
+            "name":    "Lights Electricity Rate",
+            "key":     "LIVING ZONE Lights",
+            "fmiName": "lighting output"
+          }}
+        ]
+      }}
+    }}
+  )", fmt::arg("idfpath", single_family_house_idf_path().generic_string()), fmt::arg("epwpath", chicago_epw_path().generic_string()));
+
+  const auto fmu_file_path = create_epfmu(spawn_input_string);
+  spawn::fmu::FMU fmu{fmu_file_path, false}; // don't require all symbols
+  REQUIRE(fmu.fmi.fmi2GetVersion() == std::string("2.0"));
+
+  const auto resource_path = (fmu.extractedFilesPath() / "resources").string();
+  fmi2CallbackFunctions callbacks = {fmuNothingLogger, calloc, free, NULL, NULL}; // called by the model during simulation
+  const auto comp = fmu.fmi.fmi2Instantiate("test-instance", fmi2ModelExchange, "abc-guid", resource_path.c_str(), &callbacks, false, true);
+
+  fmi2Status status; 
+
+  status = fmu.fmi.fmi2SetupExperiment(comp, false, 0.0, 0.0, false, 0.0);
+  REQUIRE(status == fmi2OK);
+
+  const auto model_description_path = fmu.extractedFilesPath() / fmu.modelDescriptionPath();
+  spawn::fmu::ModelDescription modelDescription(model_description_path);
+
+  constexpr std::array<const char*, 4> variable_names{
+    "LIVING ZONE_T",
+    "LIVING ZONE_QConSen_flow",
+    "lighting input",
+    "lighting output"
+  };
+
+  std::map<std::string, fmi2ValueReference> variable_refs;
+  for (const auto name : variable_names) {
+    variable_refs[name] = modelDescription.valueReference(name);
+  }
+
+  const auto lighting_input_ref = modelDescription.valueReference("lighting input");
+  const auto lighting_output_ref = modelDescription.valueReference("lighting output");
+
+  const std::array<fmi2ValueReference, 1> input_refs = {lighting_input_ref};
+  const std::array<fmi2ValueReference, 1> output_refs = {lighting_output_ref};
+
+  status = fmu.fmi.fmi2ExitInitializationMode(comp);
+  REQUIRE(status == fmi2OK);
+
+  std::array<fmi2Real, output_refs.size()> output_values;
+  std::array<fmi2Real, input_refs.size()> input_values;
+
+  input_values[0] = 0.0;
+  status = fmu.fmi.fmi2SetReal(comp, input_refs.data(), input_refs.size(), input_values.data());
+  CHECK(status == fmi2OK);
+
+  status = fmu.fmi.fmi2GetReal(comp, output_refs.data(), output_refs.size(), output_values.data());
+  CHECK(status == fmi2OK);
+  CHECK( output_values[0] == Approx(0.0) );
+
+  input_values[0] = 1.0;
+  status = fmu.fmi.fmi2SetReal(comp, input_refs.data(), input_refs.size(), input_values.data());
+  CHECK(status == fmi2OK);
+
+  status = fmu.fmi.fmi2GetReal(comp, output_refs.data(), output_refs.size(), output_values.data());
+  CHECK(status == fmi2OK);
+  CHECK( output_values[0] == Approx(1000.0) );
+
+  input_values[0] = 0.5;
+  status = fmu.fmi.fmi2SetReal(comp, input_refs.data(), input_refs.size(), input_values.data());
+  CHECK(status == fmi2OK);
+
+  status = fmu.fmi.fmi2GetReal(comp, output_refs.data(), output_refs.size(), output_values.data());
+  CHECK(status == fmi2OK);
+  CHECK( output_values[0] == Approx(500.0) );
+
+  input_values[0] = 0.0;
+  status = fmu.fmi.fmi2SetReal(comp, input_refs.data(), input_refs.size(), input_values.data());
+  CHECK(status == fmi2OK);
+
+  status = fmu.fmi.fmi2GetReal(comp, output_refs.data(), output_refs.size(), output_values.data());
+  CHECK(status == fmi2OK);
+  CHECK( output_values[0] == Approx(0.0) );
+
+  status = fmu.fmi.fmi2Terminate(comp);
+  REQUIRE(status == fmi2OK);
+}
+