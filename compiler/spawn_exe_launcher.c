83ed546ea4936a1c6d866d34f4560558927115c0
Kyle Benne <kyle.benne@nrel.gov>
2021-05-27 10:32:26 -0500

    Add documentation comment about what spawn_exe_launcher does
    There are no code changes in this commit. Just a record of what this
    crazy stuff does.

--- compiler/spawn_exe_launcher.c
+++ compiler/spawn_exe_launcher.c
@@ -4,6 +4,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+// The purpose of this program is the following...
+// Modelica supports a feature that enables user defined external functions,
+// which can be implemented in the C programming language. During the process of
+// compiling Modelica source, the Modelia compiler will call out to the C compiler
+// to compile user defined external functions into an executable program.
+//
+// Unfortunately, technical challenges have prevented the Spawn embedded compiler/linker
+// from generating executable programs, however it can generate shared libraries. This main
+// program, which is compiled into an exe at spawn build time, is the solution. This 
+// precompiled executable program utilizes dlopen to call the just in time compiled shared
+// library containing the user defined function. 
+//
+// See compilerchain.cpp:makeModelicalExternalFunction where this executable is copied into an FMU.
 int main(int argc, char **argv)
 {
   char so_filename[1024];
