3caf70d6ac86f057faa0a939ab06dc5e890b48f2
Kyle Benne <kyle.benne@nrel.gov>
2022-04-10 22:21:45 -0500

    Add a failing test to demonstrate fmi2Reset
    ref lbl-srg/modelica-buildings#2956

--- test/test_fmi.cpp
+++ test/test_fmi.cpp
@@ -1,5 +1,10 @@
 #include "../fmu/fmi2.hpp"
+#include "../fmu/fmu.hpp"
+#include "../fmu/logger.h"
+#include "../fmu/modeldescription.hpp"
+#include "../util/math.hpp"
 #include "../util/filesystem.hpp"
+#include "../util/config.hpp"
 #include "paths.hpp"
 #include <catch2/catch.hpp>
 #include <iostream>
@@ -27,4 +32,53 @@ TEST_CASE("Test loading of FMI missing symbols")
   REQUIRE(spawn_fs::is_regular_file(fmi_file));
 
   REQUIRE_THROWS(spawn::fmu::FMI2{example_fmu_path(), true});
+}
+
+TEST_CASE("Test Resetting a Spawn based FMU")
+{
+  const auto cmd =
+      spawnexe() + " modelica --create-fmu Buildings.ThermalZones.EnergyPlus.Validation.ThermalZone.OneZoneOneYear --fmu-type ME";
+  const auto result = system(cmd.c_str()); // NOLINT
+  REQUIRE(result == 0);
+
+  const auto fmu_path = spawn::project_binary_dir() / "Buildings_ThermalZones_EnergyPlus_Validation_ThermalZone_OneZoneOneYear.fmu";
+
+  spawn::fmu::FMU fmu{fmu_path, false};
+  CHECK(fmu.fmi.fmi2GetVersion() == std::string("2.0"));
+  const auto resource_path = std::string("file://") + (fmu.extractedFilesPath() / "resources").string();
+
+  spawn::fmu::ModelDescription modelDescription(fmu.extractedFilesPath() / fmu.modelDescriptionPath());
+
+  fmi2CallbackFunctions callbacks = {
+      fmuStdOutLogger, calloc, free, nullptr, nullptr}; // called by the model during simulation
+  const auto comp = fmu.fmi.fmi2Instantiate(
+      "test-instance", fmi2ModelExchange, modelDescription.guid().c_str(), resource_path.c_str(), &callbacks, false, true);
+
+  // Run the model for a day
+  fmi2Status status = fmu.fmi.fmi2SetupExperiment(comp, false, 0.0, 0.0, false, 0.0);
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2EnterInitializationMode(comp);
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2ExitInitializationMode(comp);
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2SetTime(comp, spawn::days_to_seconds(1));
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2Terminate(comp);
+  CHECK(status == fmi2OK);
+
+  // Reset
+  status = fmu.fmi.fmi2Reset(comp);
+  CHECK(status == fmi2OK);
+
+  // Run the model again for a day
+  status = fmu.fmi.fmi2SetupExperiment(comp, false, 0.0, 0.0, false, 0.0);
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2EnterInitializationMode(comp);
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2ExitInitializationMode(comp);
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2SetTime(comp, spawn::days_to_seconds(1));
+  CHECK(status == fmi2OK);
+  status = fmu.fmi.fmi2Terminate(comp);
+  CHECK(status == fmi2OK);
 }
