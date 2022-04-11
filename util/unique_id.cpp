62ecb7e0816fc7e343cc3206ec4b9610f8aa143f
Clang Robot <robot@example.com>
2022-04-11 03:38:58 +0000

    Committing clang-format changes

--- util/unique_id.cpp
+++ util/unique_id.cpp
@@ -6,19 +6,19 @@ std::string uniqueId()
 {
   constexpr auto len = 32;
   static const std::string_view hex_chars = "0123456789abcdef";
-  
+
   std::mt19937 gen{std::random_device{}()};
   std::string uuid;
   uuid.reserve(len);
-  
+
   while (uuid.size() < len) {
     auto n = gen();
-    for (auto i = std::mt19937::max();  i & 0x8 && uuid.size() < len;  i >>= 4) {
+    for (auto i = std::mt19937::max(); i & 0x8 && uuid.size() < len; i >>= 4) {
       uuid += hex_chars[n & 0xf];
       n >>= 4;
     }
   }
-  
+
   return uuid;
 }
 
