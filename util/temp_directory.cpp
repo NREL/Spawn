0c79df68700f0a19546e67bcb23a637592b805a3
Jason Turner <jason@emptycrate.com>
2022-09-21 09:29:18 -0600

    Attempt to address race in TempDirectory

--- util/temp_directory.cpp
+++ util/temp_directory.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <fmt/format.h>
 #include <fstream>
+#include <random>
 #include <spdlog/spdlog.h>
 
 namespace spawn::util {
@@ -11,11 +12,13 @@ namespace spawn::util {
 Temp_Directory::Temp_Directory(const std::string &t_prefix)
 {
   static constexpr auto max_tries = 1000;
+  std::random_device rd;
   for (int count = 0; count < max_tries; ++count) {
     const auto p =
         spawn_fs::temp_directory_path() /
         fmt::format(
-            "{}-{}-{}", t_prefix, std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()), count);
+            "{}-{}-{}", t_prefix, rd(), count);
+    // seems that sometimes in a race against the file system this returns true when it should not
     if (spawn_fs::create_directories(p)) {
       m_dir = p;
       return;
